293403aa00bd75dc87aac1e12ebc2d9d41f6cf5f
Brian Takita <brian.takita@gmail.com>
2023-10-29 01:54:06 -0400

    anchor:
    	+ Atb_flexmockrb
    	+ Atb_mocharb
    	+ Atb_rspec_mocks

---
+++ src/anchor/Atb_mocharb.tsx
@@ -0,0 +1,4 @@
+import { _Atb__new } from './_Atb__new'
+export const Atb_mocharb = _Atb__new(
+	'Mocha',
+	'https://github.com/freerange/mocha')
---
+++ src/anchor/Atb_rspec_mocks.tsx
@@ -0,0 +1,4 @@
+import { _Atb__new } from './_Atb__new'
+export const Atb_rspec_mocks = _Atb__new(
+	'rspec-mocks',
+	'https://github.com/rspec/rspec-mocks')
