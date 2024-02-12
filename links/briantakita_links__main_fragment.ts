0c29bf83db65166a8653b5558404e9076aba30d1
Brian Takita <brian.takita@gmail.com>
2024-02-11 22:19:02 -0500

    ./links: briantakita_links__main_fragment_: .briantakita__links_c→.briantakita_links__main_fragment

--- links/briantakita_links__main_fragment.ts
+++ links/briantakita_links__main_fragment.ts
@@ -12,7 +12,7 @@ export function briantakita_links__main_fragment_<env_T extends relement_env_T>(
 		blog__main_fragment_<env_T>({
 			ctx,
 			title: 'Links',
-			class: 'briantakita__links_c',
+			class: 'briantakita_links__main_fragment',
 		}, section_(
 			linkument_c_))
 	)
