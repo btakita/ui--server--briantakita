dfa1a511335fb1a357eeefbe9e2f8100c0ba8fd2
Brian Takita <brian.takita@gmail.com>
2024-02-18 22:52:57 -0500

    ./portfolio: portfolio__doc_html_: stack: collapse each list into space delimited

--- portfolio/portfolio__doc_html.md.ts
+++ portfolio/portfolio__doc_html.md.ts
@@ -1,12 +1,14 @@
 import { md__raw_ } from '@rappstack/ui--any--blog/md'
 import { tb_a_ } from '@rappstack/ui--any/anchor'
+import { type tag_dom_T } from 'relementjs'
 import { type request_ctx_T } from 'relysjs/server'
 import {
 	astrojs__tb_a_,
 	astropaper__tb_a_,
 	aws__tb_a_,
 	backbonejs__tb_a_,
-	briantakita_me_dev__tb_a_, brookebrodack_dev__tb_a_,
+	briantakita_me_dev__tb_a_,
+	brookebrodack_dev__tb_a_,
 	brookebrodack_net__tb_a_,
 	bunjs__tb_a_,
 	cacheable_flash__tb_a_,
@@ -27,7 +29,8 @@ import {
 	lucene__tb_a_,
 	martin_investments__tb_a_,
 	menu__tb_a_,
-	milyoni__tb_a_, motion_one__tb_a_,
+	milyoni__tb_a_,
+	motion_one__tb_a_,
 	nanostores__tb_a_,
 	neo__tb_a_,
 	news_corp__tb_a_,
@@ -64,7 +67,7 @@ export function portfolio__doc_html_({
 }:{
 	ctx:request_ctx_T
 }) {
-  return (
+	return (
 		md_layout__doc_html_({
 			ctx,
 			title: 'Portfolio',
@@ -77,36 +80,34 @@ export function portfolio__doc_html_({
 
 ${briantakita_me_dev__tb_a_()} static app with libraries forked from ${astropaper__tb_a_()}. I did some refactoring including converting it to a monorepo, used solidjs instead of react, and added ctx-core & Nano Stores. These changes improve code re-use to be used in other profile sites which I will soon work on. I plan on adding support for ${strapi__tb_a_()}, so people without technical experience can write posts on their own optimized blog.
 
-Stack:
-
-* ${rappstack__tb_a_()}
-* ${bunjs__tb_a_()}
-* ${elysiajs__tb_a_()}
-* ${ctx_core__tb_a_()}
-* ${relysjs__tb_a_()}
-* ${relementjs__tb_a_()}
-* ${rmemo__tb_a_()}
-* ${docker__tb_a_()}
-* VPS
+${stack__a_a1_(
+	rappstack__tb_a_(),
+	bunjs__tb_a_(),
+	elysiajs__tb_a_(),
+	ctx_core__tb_a_(),
+	relysjs__tb_a_(),
+	relementjs__tb_a_(),
+	rmemo__tb_a_(),
+	docker__tb_a_(),
+	'VPS')}
 
 ## ${brookebrodack_net__tb_a_()}
 
 Brooke Brodack is one of the original YouTube personalities. Her character, Brookers, once had the most subscribers on YouTube. This site features a Brookers Timeline & Brooke Brodack's current Youtube Content...embedding the YouTube & Internet Archive players. rmemo shines in this implementation to manage the player state & web animations. I was able to improve the web animations after switching from ${motion_one__tb_a_()} to rmemo. The ${brookebrodack_dev__tb_a_('source code')} is under the Apache 2 license.
 
-Stack:
-
-* ${rappstack__tb_a_()}
-* ${bunjs__tb_a_()}
-* ${sqlite__tb_a_()}
-* ${drizzle_orm__tb_a_()}
-* ${elysiajs__tb_a_()}
-* ${hyop__tb_a_()}
-* ${ctx_core__tb_a_()}
-* ${relysjs__tb_a_()}
-* ${relementjs__tb_a_()}
-* ${rmemo__tb_a_()}
-* ${docker__tb_a_()}
-* VPS
+${stack__a_a1_(
+	rappstack__tb_a_(),
+	bunjs__tb_a_(),
+	sqlite__tb_a_(),
+	drizzle_orm__tb_a_(),
+	elysiajs__tb_a_(),
+	hyop__tb_a_(),
+	ctx_core__tb_a_(),
+	relysjs__tb_a_(),
+	relementjs__tb_a_(),
+	rmemo__tb_a_(),
+	docker__tb_a_(),
+	'VPS')}
 
 ## ${martin_investments__tb_a_()}
 
@@ -116,16 +117,16 @@ Utilized various financial APIs. Built a XLSX exporter spanning > 10000 rows & h
 
 Built SVG & PDF exporters of portfolio & company ESG reports.
 
-Stack:
-
-* ${nodejs__tb_a_()}, ${pnpm__tb_a_()}
-* ${ctx_core__tb_a_()}
-* ${astrojs__tb_a_()}
-* ${nanostores__tb_a_()}
-* ${solidjs__tb_a_()}
-* ${redis__tb_a_()}
-* ${docker__tb_a_()}
-* VPS
+${stack__a_a1_(
+	nodejs__tb_a_(),
+	pnpm__tb_a_(),
+	ctx_core__tb_a_(),
+	astrojs__tb_a_(),
+	nanostores__tb_a_(),
+	solidjs__tb_a_(),
+	redis__tb_a_(),
+	docker__tb_a_(),
+	'VPS')}
 
 ## ${censible__tb_a_()}
 
@@ -135,50 +136,47 @@ Progressive Web App that enables the client to upload their portfolio from a CSV
 
 Market survey of Women in corporate leadership positions. Scoring methodology applied to Aggregate analysis, Best Performing Companies, Best Performing Large Companies, Worst Performing Companies, Best & Worst Performing Funds, Largest Funds, & Socially Responsible Funds.
 
-Stack:
-
-* ${nodejs__tb_a_()}, ${pnpm__tb_a_()}
-* ${ctx_core__tb_a_()}
-* ${astrojs__tb_a_()}
-* ${nanostores__tb_a_()}
-* ${solidjs__tb_a_()}
-* ${solid_start__tb_a_()}
-* ${postgres__tb_a_()}
-* ${redis__tb_a_()}
-* ${heroku__tb_a_()}
+${stack__a_a1_(
+	nodejs__tb_a_(),
+	pnpm__tb_a_(),
+	ctx_core__tb_a_(),
+	astrojs__tb_a_(),
+	nanostores__tb_a_(),
+	solidjs__tb_a_(),
+	solid_start__tb_a_(),
+	postgres__tb_a_(),
+	redis__tb_a_(),
+	heroku__tb_a_())}
 
 ## ${menu__tb_a_()}
 
 Front end development of the menu.com multi-tenant app. Consumer web app, restaurant back-office, & back-office Cordova app.
 
-Stack:
-
-* ${sveltejs__tb_a_()}
-* ${ctx_core__tb_a_()}
-* ${sapper__tb_a_()}
-* ${cordova__tb_a_()}
-* ${aws__tb_a_()}
+${stack__a_a1_(
+	sveltejs__tb_a_(),
+	ctx_core__tb_a_(),
+	sapper__tb_a_(),
+	cordova__tb_a_(),
+	aws__tb_a_())}
 
 ## ${foresight_host__tb_a_()}
 
 Private NextCloud groupware site with some custom plugins.
 
-Stack:
-
-* ${nextcloud__tb_a_()}
-* ${docker__tb_a_()}
-* VPS
+${stack__a_a1_(
+	nextcloud__tb_a_(),
+	docker__tb_a_(), 
+	'VPS')}
 
 ## ${neo__tb_a_()}
 
 Worked with the previous incarnation as an inclubator & consulting firm of Neo before it's acquisition. The client was Tenfore Holdings, an investment fund. The project was a low-code document writer with integrated search into the client's ${salesforce__tb_a_()} using Salesforce cloud.
 
-Stack:
-
-* ${nodejs__tb_a_()}
-* ${riotjs__tb_a_()}
-* ${rethinkdb__tb_a_()}
-* ${heroku__tb_a_()}
+${stack__a_a1_(
+	nodejs__tb_a_(),
+	riotjs__tb_a_(),
+	rethinkdb__tb_a_(),
+	heroku__tb_a_())}
 
 ## Rundavoo
 
@@ -186,63 +184,68 @@ Web front-end development for social planning & decision app for restaurant patr
 
 Web front-end development for social planning & decision app for restaurant patrons. Mobile cordova app was included in the
 
-Stack:
-
-* ${riotjs__tb_a_()}
-* ${backbonejs__tb_a_()}
-* ${heroku__tb_a_()}
+${stack__a_a1_(
+	riotjs__tb_a_(),
+	backbonejs__tb_a_(), 
+	heroku__tb_a_())}
 
 ## ${milyoni__tb_a_()}
 
 Consulting developer for social entertainment platform. Developed scalable live video events with admin message moderation & components, on-demand movies with time-coded chat & interactive components.
 
-Stack:
-
-* ${ruby_on_rails__tb_a_()}
-* ${nodejs__tb_a_()}
-* ${postgres__tb_a_}
-* ${redis__tb_a_()}
-* ${heroku__tb_a_()}
+${stack__a_a1_(
+	ruby_on_rails__tb_a_(),
+	nodejs__tb_a_(),
+	postgres__tb_a_(), 
+	redis__tb_a_(),
+	heroku__tb_a_())}
 
 ## ${honk__tb_a_()}
 
 Lead developer for automotive pricing & info startup. Built product from initial concept through acquisition by ${truecar__tb_a_()}. Key partnerships include ${news_corp__tb_a_()}, ${wsj__tb_a_()}, ${usaa__tb_a_()}.
 
-Stack:
-
-* ${ruby_on_rails__tb_a_()}
-* ${sinatrarb__tb_a_()}
-* ${erector__tb_a_()}
-* ${backbonejs__tb_a_()}
-* ${postgres__tb_a_()}
-* ${lucene__tb_a_()}
-* ${engine_yard__tb_a_()}
+${stack__a_a1_(
+	ruby_on_rails__tb_a_(),
+	sinatrarb__tb_a_(),
+	erector__tb_a_(),
+	backbonejs__tb_a_(), 
+	postgres__tb_a_(),
+	lucene__tb_a_(),
+	engine_yard__tb_a_()
+)}
 
 ## Pivotal Labs
 
 Agile engineer with Web2.0 consulting firm & startup incubator from 2005-2009. Pivotal Labs was acquired by EMC & became VM Ware Tunzu after I left. At Pivotal, I worked with > 20 startup companies, bootstrapping their product development, engineer process, & mentoring developers. While at Pivotal Labs I worked on open source libraries including:
 
-* ${rspec__tb_a_()}
-* ${rr__tb_a_()}
-* ${erector__tb_a_()}
-* ${unison__tb_a_()}
-* ${cacheable_flash__tb_a_()}
+${stack__a_a1_(
+	rspec__tb_a_(), 
+	rr__tb_a_(),
+	erector__tb_a_(),
+	unison__tb_a_(),
+	cacheable_flash__tb_a_()
+)}
 
 I spoke at the ${tb_a_({ href: 'https://www.oreilly.com/pub/pr/1966' }, `2008 Rails Conf`)}.
 
 Stack:
 
-* ${ruby_on_rails__tb_a_()}
-* ${sinatrarb__tb_a_()}
-* ${erector__tb_a_()}
-* ${backbonejs__tb_a_()}
-* ${jquery__tb_a_()}
-* ${postgres__tb_a_()}
-* ${lucene__tb_a_()}
-* ${aws__tb_a_()}
-* ${heroku__tb_a_()}
+${stack__a_a1_(
+	ruby_on_rails__tb_a_(),
+	sinatrarb__tb_a_(),
+	erector__tb_a_(),
+	backbonejs__tb_a_(), 
+	jquery__tb_a_(),
+	postgres__tb_a_(),
+	lucene__tb_a_(),
+	aws__tb_a_(),
+	heroku__tb_a_()
+)}
 `.trim())
 			// @formatter:on
 		])
 	)
+}
+function stack__a_a1_(...a_a1:tag_dom_T[]) {
+	return a_a1.join(' ')
 }
