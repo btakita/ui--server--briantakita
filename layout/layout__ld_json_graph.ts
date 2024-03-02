aad7296834492ea76cf81551bdf30aa868511266
Brian Takita <brian.takita@gmail.com>
2024-03-02 01:13:20 -0500

    ./layout: layout__ld_json_graph:
    	root_id→root_Person_id
    	root_summary_id→root_AboutPage_id
    	about_id→about_WebPage_id

--- layout/layout__ld_json_graph.ts
+++ layout/layout__ld_json_graph.ts
@@ -10,41 +10,48 @@ export const [
 ] = id_be_memo_pair_(
 	'layout__ld_json_graph',
 	(ctx:request_ctx_T)=>{
-		const root_id = request_url__origin_(ctx)
-		const root_summary_id = url__join(root_id, '#summary')
-		const about_id = url__join(request_url__origin_(ctx), 'about')
+		const root_Person_id = request_url__origin_(ctx)
+		const root_AboutPage_id = url__join(root_Person_id, '#about')
+		// const root_Project_rmemo_id = url__join(root_Person_id, '#rmemo')
+		const about_WebPage_id = url__join(request_url__origin_(ctx), 'about')
 		return <Graph>{
 			'@context': 'https://schema.org',
 			'@graph': [
 				{
 					'@type': 'Person',
-					'@id': root_id,
+					'@id': root_Person_id,
 					name: site__author_(ctx),
 					hasOccupation: {
 						'@type': 'Occupation',
 						name: 'Full Stack Developer',
 						qualifications: 'Develops many interoperable apps + open source libraries that are simple & small in size. Creates flat architectures that scale large & small. These libraries & apps build on each other. Reactive programming with general purpose contexts. Precise & scalable domain ontology. These techniques remove incidental complexity & bloat that come with other programming paradigms.',
 					},
-					mainEntityOfPage: about_id,
-					subjectOf: about_id,
+					mainEntityOfPage: about_WebPage_id,
+					subjectOf: about_WebPage_id
 				},
 				{
 					'@type': 'AboutPage',
-					'@id': root_summary_id,
+					'@id': root_AboutPage_id,
 					name: site__author_(ctx) + ' Summary',
 					inLanguage: 'en-us',
-					isPartOf: { '@id': root_id }
+					isPartOf: { '@id': root_Person_id }
 				},
 				{
 					'@type': 'WebPage',
-					'@id': about_id,
-					url: about_id,
+					'@id': about_WebPage_id,
+					url: about_WebPage_id,
 					name: 'About | ' + site__author_(ctx),
 					inLanguage: 'en-us',
-					isPartOf: { '@id': root_summary_id },
-					about: { '@id': root_id },
-					mainEntity: { '@id': root_id }
-				}
+					isPartOf: { '@id': root_AboutPage_id },
+					about: { '@id': root_Person_id },
+					mainEntity: { '@id': root_Person_id }
+				},
+				// {
+				// 	'@type': 'Project',
+				// 	'@id': root_Project_rmemo_id,
+				// 	name: 'rmemo | ' + site__author_(ctx),
+				// 	// url:
+				// }
 			]
 		}
 	})
