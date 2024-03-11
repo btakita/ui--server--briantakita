d42d259127654e3832698fa73826eccfb49e4ad7
Brian Takita <brian.takita@gmail.com>
2024-03-11 03:54:00 -0400

    ./search: search__doc_html_:
    	∋ WebPage__description__set
    	∋ WebPage__headline__set
    	∋ WebPage__name__set
    	∋ WebPage__type__set
    	+ description

--- search/search__doc_html.ts
+++ search/search__doc_html.ts
@@ -1,4 +1,10 @@
-import { site__title_ } from '@rappstack/domain--server/site'
+import {
+	WebPage__description__set,
+	WebPage__headline__set,
+	WebPage__name__set,
+	WebPage__type__set
+} from '@rappstack/domain--server/jsonld'
+import { site__author_, site__title_ } from '@rappstack/domain--server/site'
 import { blog_search__main_fragment_ } from '@rappstack/ui--server--blog/search'
 import { type request_ctx_T } from 'relysjs/server'
 import { briantakita__footer_ } from '../footer/index.js'
@@ -9,18 +15,23 @@ export function search__doc_html_({
 }:{
 	ctx:request_ctx_T
 }) {
+	const title = 'Search | ' + site__title_(ctx)
+	const description = 'Search through blog posts authored by ' + site__author_(ctx)
+	WebPage__name__set(ctx, title)
+	WebPage__headline__set(ctx, title)
+	WebPage__description__set(ctx, description)
+	WebPage__type__set(ctx, 'SearchResultsPage')
 	return (
 		layout__doc_html_({
 			ctx,
-			title: 'Search | ' + site__title_(ctx)
+			title,
+			description,
 		}, [
 			briantakita__header_({
 				ctx,
 				active_link: 'search'
 			}),
-			blog_search__main_fragment_({
-				ctx,
-			}),
+			blog_search__main_fragment_({ ctx, }),
 			briantakita__footer_({ ctx })
 		])
 	)
