8d29755fb75c9ac257e549d930b9fddd00178f92
Brian Takita <brian.takita@gmail.com>
2024-03-22 20:15:19 -0400

    ./post: post__doc_html_: ∋ blog__main_fragment_:
    	+ h1_class: text-4xl
    	+ h2_class: text-2xl

--- post/post__doc_html.ts
+++ post/post__doc_html.ts
@@ -41,6 +41,8 @@ export function post__doc_html_({
 				progress_class: 'bg-skin-accent',
 				author_id_ref: Person_id_ref_(ctx),
 				image: Person_image,
+				h1_class: 'text-4xl',
+				h2_class: 'text-2xl',
 			}),
 			briantakita__footer_({ ctx }),
 		])
